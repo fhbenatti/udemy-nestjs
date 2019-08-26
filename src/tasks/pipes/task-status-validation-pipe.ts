0b729521087dfa03155563d26c6affd813ad4a90
Fábio Benatti <fhbenatti@gmail.com>
2019-08-25 21:50:39 -0300

    add typeorm operations

--- src/tasks/pipes/task-status-validation-pipe.ts
+++ src/tasks/pipes/task-status-validation-pipe.ts
@@ -3,7 +3,7 @@ import {
   ArgumentMetadata,
   BadRequestException,
 } from '@nestjs/common';
-import { TaskStatus } from '../tasks.model';
+import { TaskStatus } from '../tasks-status.enum';
 import { TasksController } from '../tasks.controller';
 
 export class TaskStatusValidationPipe implements PipeTransform {
