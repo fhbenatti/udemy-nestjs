6bf76d8cd6a08518b69dcc2e7f7d1728d1688396
Fábio Benatti <fhbenatti@gmail.com>
2019-09-11 21:18:00 -0300

    get tasks; add user module

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -22,14 +22,12 @@ import { TaskStatus } from './tasks-status.enum';
 export class TasksController {
   constructor(private taskService: TasksService) {}
 
-  // @Get()
-  // getTasks(@Query(ValidationPipe) filterDto: GetTasksFilterDto): Task[] {
-  //   if (Object.keys(filterDto).length) {
-  //     return this.taskService.getTasksWithFilters(filterDto);
-  //   } else {
-  //     return this.taskService.getAllTasks();
-  //   }
-  // }
+  @Get()
+  getTasks(
+    @Query(ValidationPipe) filterDto: GetTasksFilterDto,
+  ): Promise<Task[]> {
+    return this.taskService.getTasks(filterDto);
+  }
 
   @Get('/:id')
   getTaskById(@Param('id', ParseIntPipe) id: number): Promise<Task> {
